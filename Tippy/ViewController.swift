46aef64c47a862e4c0d0b32ff0c0fb6663f408be
Andy Taylor <andytlr@gmail.com>
2015-10-08 13:25:35 -0700

    Initial

--- Tippy/ViewController.swift
+++ Tippy/ViewController.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var billAmountInput: UITextField!
+    
+    @IBOutlet weak var tipPercentageSegmentedController: UISegmentedControl!
+    
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    
+    @IBOutlet weak var billTotalLabel: UILabel!
+    
+    func formatMoney(cash: Double) -> String {
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .CurrencyStyle
+        return formatter.stringFromNumber(cash)!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipAmountLabel.text = "$0.00"
+        billTotalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func billAmountInputChanged(sender: AnyObject) {
+        print("LOL")
+        
+        var tipAmounts = [15.0, 18.0, 20.0]
+        
+        let tipPercentage = tipAmounts[tipPercentageSegmentedController.selectedSegmentIndex]
+        
+        let bill = NSString(string: billAmountInput.text!).doubleValue
+        let tip = (bill / 100) * tipPercentage
+        let billTotal = bill + tip
+        
+        tipAmountLabel.text = "\(formatMoney(tip))"
+        billTotalLabel.text = "\(formatMoney(billTotal))"
+    
+    }
 
-
+    @IBAction func tapBackground(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    
 }
 
