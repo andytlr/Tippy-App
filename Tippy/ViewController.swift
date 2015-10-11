3f0120385422252d908ae1c2d2b0cd968de70068
Andy Taylor <andytlr@gmail.com>
2015-10-11 12:24:17 -0700

    Formatting. Commenting. Use an iterator to set segmented control labels.

--- Tippy/ViewController.swift
+++ Tippy/ViewController.swift
@@ -11,79 +11,88 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Variables for all the UI elements I need to change
     @IBOutlet weak var tipPercentageSegmentedController: UISegmentedControl!
-    
     @IBOutlet weak var billAmountInput: UITextField!
-    
     @IBOutlet weak var introLabel: UITextView!
-    
     @IBOutlet weak var tipAmountLabel: UILabel!
-    
     @IBOutlet weak var billTotalLabel: UILabel!
-    
     @IBOutlet weak var percentageLabel: UILabel!
-    
     @IBOutlet weak var resultsView: UIView!
-    
     @IBOutlet weak var wholeResultsView: UIView!
     
+    // Function to format a double as money
     func formatMoney(cash: Double) -> String {
         let formatter = NSNumberFormatter()
         formatter.numberStyle = .CurrencyStyle
         return formatter.stringFromNumber(cash)!
     }
     
+    // Round a double to one decimal place
     func roundToOneDecimalPlace(float: Double) -> Double {
         return round(float * 10) / 10
     }
     
+    // Tip amounts and labels
     let tipAmounts = [15.0, 18.0, 20.0]
-//    var tippieAmounts = ["Ok": 15.0, "Good": 18.0, "Great": 20.0]
+    let tipLabels = ["Ok", "Good", "Great"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+
+        // Set light status bar
         UIApplication.sharedApplication().statusBarStyle = .LightContent
         
+        // Focus the bill amount input
         billAmountInput.becomeFirstResponder()
         
-        tipPercentageSegmentedController.setTitle(">\(Int(tipAmounts[0]))% Ok", forSegmentAtIndex: 0)
-        tipPercentageSegmentedController.setTitle(">\(Int(tipAmounts[1]))% Good", forSegmentAtIndex: 1)
-        tipPercentageSegmentedController.setTitle(">\(Int(tipAmounts[2]))% Great", forSegmentAtIndex: 2)
+        // Set labels for segmented control
+        var segIndex = 0
+        while segIndex < tipAmounts.count {
+            tipPercentageSegmentedController.setTitle(">\(Int(tipAmounts[segIndex]))% \(tipLabels[segIndex])", forSegmentAtIndex: segIndex)
+            segIndex++
+        }
         
+        // Use tabular figures for results amounts
         tipAmountLabel.font = UIFont.monospacedDigitSystemFontOfSize(24, weight: UIFontWeightThin)
         billTotalLabel.font = UIFont.monospacedDigitSystemFontOfSize(24, weight: UIFontWeightThin)
         
+        // Move and hide stuff
         billAmountInput.transform = CGAffineTransformMakeTranslation(0, 80)
         wholeResultsView.transform = CGAffineTransformMakeTranslation(0, 180)
         wholeResultsView.alpha = 0
-        
         introLabel.alpha = 0
         introLabel.transform = CGAffineTransformMakeTranslation(0, 290)
 
+        // Animate in NUX after delay
         UIView.animateWithDuration(0.15, delay: 5.0, options: [], animations: {
             self.introLabel.alpha = 1
         }, completion: nil)
     }
 
+    // ¯\_(ツ)_/¯ I didn't write this. Something about memory warnings.
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
+    // Whenever the input changes. Do all the things.
     @IBAction func billAmountInputChanged(sender: AnyObject) {
         
+        // Set the tip percentage to use to the one selected in the segmented control
         let tipPercentage = tipAmounts[tipPercentageSegmentedController.selectedSegmentIndex]
         
+        // Strip the dollar sign from the input text then get a double out of it
         let billWithDollarStripped = billAmountInput.text!.stringByReplacingOccurrencesOfString("$", withString: "")
-        
         let bill = NSString(string: billWithDollarStripped).doubleValue
+        
+        // Calculate tip and total bill
         let tip = (bill / 100) * tipPercentage
         let billTotal = bill + tip
         
+        // Initialize empty double
         var roundUpInIncrementsOf = 0.0
         
+        // Set what increments to round up in depending on the total.
         switch billTotal {
         case 1...10:
             roundUpInIncrementsOf = 0.50
@@ -97,55 +106,94 @@ class ViewController: UIViewController {
             roundUpInIncrementsOf = 10.0
         }
         
+        // Get the final bill including rounding
         let roundedBillTotal = (billTotal - (billTotal % roundUpInIncrementsOf)) + roundUpInIncrementsOf
+        
+        // And the tip amount
         let roundedTipAmount = tip + (roundedBillTotal - billTotal)
+        
+        // And the percentage you'll actually pay. E.g. 19.3% instead of 18%
         let roundedTipPercent = roundToOneDecimalPlace((roundedTipAmount / bill) * 100)
+        // Format it
         let roundedTipPercentStringified = String(roundedTipPercent).stringByReplacingOccurrencesOfString(".0", withString: "")
+        
+        // The original tip formatted nicely
         let tipPercentStringified = String(tipPercentage).stringByReplacingOccurrencesOfString(".0", withString: "")
+        
+        // How much extra you'll pay because of rounding
         let extraPaid = roundedBillTotal - billTotal
         
+        // If the input is effectively empty
         if billAmountInput.text == "" || billAmountInput.text == "$" || billAmountInput.text == "$0" || billAmountInput.text == "$." {
+            
+            // Move the input bouncy style
             UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 0.55, initialSpringVelocity: 0.3, options: [], animations: {
                 self.billAmountInput.transform = CGAffineTransformMakeTranslation(0, 80)
             }, completion: nil)
+            
+            // Move the results view out of view
             UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 0.75, initialSpringVelocity: 0.3, options: [], animations: {
                 self.wholeResultsView.transform = CGAffineTransformMakeTranslation(0, 180)
                 }, completion: nil)
+            
+            // Hide the results view.
+            // Was strange seeing the results if you shook the phone and the keyboard dismisssed. Or behind the black transparent keyboard.
+            UIView.animateWithDuration(0, delay: 0.2, options: [], animations: {
+                self.wholeResultsView.alpha = 0
+                }, completion: nil)
+            
+            // Show the NUX tip after a delay
             UIView.animateWithDuration(0.15, delay: 5.0, options: [], animations: {
                 self.introLabel.alpha = 1
             }, completion: nil)
-            UIView.animateWithDuration(0, delay: 0.2, options: [], animations: {
-                self.wholeResultsView.alpha = 0
-            }, completion: nil)
+            
         } else {
+            
+            // Bring the results view into view and move the input up.
             UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 0.75, initialSpringVelocity: 0.3, options: [], animations: {
                 self.billAmountInput.transform = CGAffineTransformMakeTranslation(0, 0)
                 self.wholeResultsView.transform = CGAffineTransformMakeTranslation(0, 0)
             }, completion: nil)
+            
+            // hide intro label
             self.introLabel.alpha = 0
+            
+            // Show results view
             wholeResultsView.alpha = 1
+            
+            // Set tip and bill amounts
             tipAmountLabel.text = "\(formatMoney(roundedTipAmount))"
             billTotalLabel.text = "\(formatMoney(roundedBillTotal))"
+            
+            // Was getting problems where the percentage was infinite when the input was empty so...
             if billAmountInput.text != "" || bill == 0 {
+                
+                // Don't think I need this...
                 percentageLabel.hidden = false
+                
+                // Sentence that explains what's happening under results.
                 percentageLabel.text = "That's \(roundedTipPercentStringified)%, only \(formatMoney(extraPaid)) extra than \(tipPercentStringified)%."
             }
         }
         
+        // Make sure you can't type stupid shit in the input.
+        // You can still do some stupid shit but I can't make it crash.
+        // Probably need regex or something to limit the type of input the input will accept.
         if billAmountInput.text == "" || billAmountInput.text == "$0" || billAmountInput.text == "$." {
             billAmountInput.text = "$"
         }
     
     }
     
+    // Swipe down on the whole view...
     @IBAction func swipeView(sender: AnyObject) {
+        
+        // Reset the input. For some reason I need both of these.
         billAmountInput.text = "$"
         billAmountInputChanged(NSString(string: "$"))
+        
+        // Hide the intro label
         introLabel.alpha = 0
-    }
-
-    @IBAction func tapBackground(sender: AnyObject) {
-//        view.endEditing(true)
     }
 }
 
